7a558063d37d6dd9071cbd4642feda9c496070d6
Pointless-simulation <48370877+Pointless-simulation@users.noreply.github.com>
2019-03-09 02:45:13 +0000

    Delete roleColourChanger.plugin.js

--- roleColourChanger.plugin.js
+++
@@ -1,8 +0,0 @@
-﻿//META{"name":"roleColourChanger","website":"https://github.com/Pointless-simulation/ColourChanger","source":"https://github.com/Pointless-simulation/ColourChanger/blob/master/rolecolourchanger.js"}*//
-
-class roleColourChanger {
-  getName() {return "Role colour changer";}
-  getDescription() {return "Set your own name colour locally.";}
-  getVersion() {return "0.1";}
-  getAuthor() {return "We are in a pointless simulation";}
-}
