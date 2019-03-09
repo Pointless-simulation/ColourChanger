2a1eee2e44417ed8a8c1c1e6d52f5c126d745f38
Pointless-simulation <48370877+Pointless-simulation@users.noreply.github.com>
2019-03-09 01:30:30 +0000

    Add files via upload

---
+++ rolecolourchanger.js
@@ -0,0 +1,20 @@
+﻿//META{"name":"Role colour changer","website":"https://github.com/Pointless-simulation/BetterDiscordPlugins/Plugins/colourchanger","source":"https://github.com/Pointless-simulation/BetterDiscordPlugins/Plugins/colourchanger/rolecolourchanger.js"}*//
+
+
+class NoDeleteMessages {
+  getName() {
+    return "Role colour changer";
+  }
+  getShortName() {
+    return "Role colour changer";
+  }
+  getDescription() {
+    return "Set your own name colour locally.";
+  }
+  getVersion() {
+    return "0.1";
+  }
+  getAuthor() {
+    return "We are in a pointless simulation";
+  }
+}
